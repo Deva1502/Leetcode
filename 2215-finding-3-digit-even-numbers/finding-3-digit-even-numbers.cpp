264c18ce4aa4fc34cbe6224e6b7b2dd073b1cb1f
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-05-13 12:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (87.00%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,21 +1,53 @@
+// class Solution {
+// public:
+//     vector<int> findEvenNumbers(vector<int>& a) {
+//         vector<int> ans;
+//         int n = a.size();
+//         set<int> s;
+//         for (int i = 0; i < n; i++) {
+//             if (a[i] == 0) continue;
+//             for (int j = 0; j < n; j++) {
+//                 if (j == i) continue;
+//                 for (int k = 0; k < n; k++) {
+//                     if (k == i || k == j) continue;
+//                     int num = a[i]*100 + a[j]*10 + a[k];
+//                     if (num % 2 == 0) s.insert(num);
+//                 }
+//             }
+//         }
+//         for (int num : s) ans.push_back(num);
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& a) {
         vector<int> ans;
-        int n = a.size();
-        set<int> s;
-        for (int i = 0; i < n; i++) {
-            if (a[i] == 0) continue;
-            for (int j = 0; j < n; j++) {
-                if (j == i) continue;
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) continue;
-                    int num = a[i]*100 + a[j]*10 + a[k];
-                    if (num % 2 == 0) s.insert(num);
+        int freq[10] = {0};
+        
+        // Count digit frequencies
+        for (int d : a) freq[d]++;
+        
+        // Check all 3-digit even numbers
+        for (int num = 100; num <= 999; num += 2) {
+            int x = num;
+            int tempFreq[10] = {0};
+            tempFreq[x % 10]++; x /= 10;
+            tempFreq[x % 10]++; x /= 10;
+            tempFreq[x]++;
+            
+            bool valid = true;
+            for (int d = 0; d < 10; d++) {
+                if (tempFreq[d] > freq[d]) {
+                    valid = false;
+                    break;
                 }
             }
+            if (valid) ans.push_back(num);
         }
-        for (int num : s) ans.push_back(num);
+        
         return ans;
     }
 };
