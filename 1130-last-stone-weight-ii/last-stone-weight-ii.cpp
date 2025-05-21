c1d9f86c8433b876d3b85a98b40e7942ae45ac55
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-05-21 14:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (81.57%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    int totalsum=0;
-    int solve(vector<int>&stones,int i,int sum){
-        //base case
-        if(i>=stones.size()){
-            int g1=sum;
-            int g2=totalsum-sum;
-            return abs(g1-g2);
-        }
-        int ans1=solve(stones,i+1,sum+stones[i]);
-        int ans2=solve(stones,i+1,sum);
-        return min(ans1,ans2);
-    }
-    int solveUsingMem(vector<int>&stones,int i,int sum,vector<vector<int>>&dp){
-        //base case
-        if(i>=stones.size()){
-            int g1=sum;
-            int g2=totalsum-sum;
-            return abs(g1-g2);
-        }
-        if(dp[i][sum]!=-1){
-            return dp[i][sum];
-        }
-        int ans1=solveUsingMem(stones,i+1,sum+stones[i],dp);
-        int ans2=solveUsingMem(stones,i+1,sum,dp);
-        dp[i][sum]=min(ans1,ans2);
-        return dp[i][sum];
-    }
     int lastStoneWeightII(vector<int>& stones) {
-        for(int i=0;i<stones.size();i++){
-            totalsum+=stones[i];
-        }
-        // return solve(stones,0,0);
+        int total = accumulate(stones.begin(), stones.end(), 0);
+        int target = total / 2;
 
-        int n=stones.size();
-        vector<vector<int>>dp(n+1,vector<int>(totalsum+1,-1));
-        return solveUsingMem(stones,0,0,dp);
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int stone : stones) {
+            for (int i = target; i >= stone; --i) {
+                dp[i] = dp[i] or dp[i - stone];
+            }
+        }
+
+        for (int i = target; i >= 0; --i) {
+            if (dp[i]) {
+                return total - (i * 2);
+            }
+        }
+
+        return -1;
     }
 };
