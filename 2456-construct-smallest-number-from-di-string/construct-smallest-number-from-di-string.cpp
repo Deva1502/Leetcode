a3cceb551c09f949feaa3bf28775f96db46c8878
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-06-07 15:15:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (26.50%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,28 +1,45 @@
+// class Solution {
+// public:
+// bool check (string p,string s){
+//     for(int i = 0;i<s.size();i++){
+//         if(s[i]=='I'){
+//             if(p[i]>=p[i+1])return false;
+//         }
+//         else{
+//             if(p[i]<p[i+1])return false;
+//         }
+//     }
+//     return true;
+// }
+//     string smallestNumber(string pattern) {
+//         int n = pattern.size();
+//         int inc = count(pattern.begin(), pattern.end(), 'I');
+//         int dec = n - inc;
+//         string per = "";
+//         for(int i = 1;i<=n+1;i++){
+//             per += (i+'0');
+//         }
+//         while(!check(per,pattern)){
+//             next_permutation(per.begin(),per.end());
+//         }
+//         return per;
+
+//     }
+// };
 class Solution {
 public:
-bool check (string p,string s){
-    for(int i = 0;i<s.size();i++){
-        if(s[i]=='I'){
-            if(p[i]>=p[i+1])return false;
-        }
-        else{
-            if(p[i]<p[i+1])return false;
-        }
-    }
-    return true;
-}
     string smallestNumber(string pattern) {
-        int n = pattern.size();
-        int inc = count(pattern.begin(), pattern.end(), 'I');
-        int dec = n - inc;
-        string per = "";
-        for(int i = 1;i<=n+1;i++){
-            per += (i+'0');
+        string res = "";
+        stack<int> st;
+        for (int i = 0; i <= pattern.size(); i++) {
+            st.push(i + 1);
+            if (i == pattern.size() || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    res += to_string(st.top());
+                    st.pop();
+                }
+            }
         }
-        while(!check(per,pattern)){
-            next_permutation(per.begin(),per.end());
-        }
-        return per;
-
+        return res;
     }
 };
