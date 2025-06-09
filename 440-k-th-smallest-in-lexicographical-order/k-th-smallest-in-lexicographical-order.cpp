ee2e2343db8676479196808dd2475386f56add23
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-06-09 14:14:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (9.23%) - LeetSync

---
+++ 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int calcSteps(int n, long long curr, long long next) {
+        int steps = 0;
+        while (curr <= n) {
+            steps += min((long long)(n + 1), next) - curr;
+            curr *= 10;
+            next *= 10;
+        }
+        return steps;
+    }
+
+    int findKthNumber(int n, int k) {
+        int curr = 1;
+        k -= 1;
+
+        while (k > 0) {
+            int steps = calcSteps(n, curr, curr + 1);
+            if (steps <= k) {
+                curr++;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k -= 1;
+            }
+        }
+
+        return curr;
+    }
+};
