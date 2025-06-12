9e10ca4f3dae2c440c72eac7a14d0681502409af
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-06-12 18:40:36 +0530

    Time: 8 ms (89.85%) | Memory: 17.2 MB (97.80%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -14,6 +14,7 @@ public:
         if(root==p || root==q)return root;
         TreeNode *lft = lowestCommonAncestor(root->left,p,q);
         TreeNode *rgt = lowestCommonAncestor(root->right,p,q);
+        //left me jayenge aur right me jayenge agar dono side mila to lca root hoga warna jis side p mila ya q mila vhi hoga
         if(lft!=NULL and rgt!=NULL)return root;
         if(lft!=NULL)return lft;
         return rgt;
