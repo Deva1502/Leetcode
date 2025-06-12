e90a71d3ccfebb4321e9204f55f5e54b2b4d8926
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-06-12 15:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (62.93%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,24 +11,34 @@
  */
 class Solution {
 public:
+void preorder(TreeNode*root,int node,vector<int>&a){
+    if(root==nullptr)return;
+    if(a.size()<node)a.push_back(root->val);
+    preorder(root->right,node+1,a);
+    preorder(root->left,node+1,a);
+}
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
         if(root==nullptr)return ans;
         //level order traversal
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int n = q.size();
-            int x = 0;
-            for(int i = 0;i<n;i++){
-                auto it = q.front();
-                q.pop();
-                x = it->val;
-                if(it->left)q.push(it->left);
-                if(it->right)q.push(it->right);
-            }
-            ans.push_back(x);
-        }
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int n = q.size();
+        //     int x = 0;
+        //     for(int i = 0;i<n;i++){
+        //         auto it = q.front();
+        //         q.pop();
+        //         x = it->val;
+        //         if(it->left)q.push(it->left);
+        //         if(it->right)q.push(it->right);
+        //     }
+        //     ans.push_back(x);
+        // }
+
+        //dfs
+        preorder(root,1,ans);
+
         return ans;
     }
 };
