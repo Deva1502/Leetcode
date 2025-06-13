be66d4c79b7ea362e59ae54b0b793e0b40c89c69
Devansh Mishra <124711862+Deva1502@users.noreply.github.com>
2025-06-13 09:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (46.35%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -6,27 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isonepre(TreeNode *root){
-        if(root==nullptr)return false;
-        if(root->val==1)return true;
-        return isonepre(root->left)||isonepre(root->right);
+    bool isonepre(TreeNode* root) {
+        if (root == nullptr)
+            return false;
+        if (root->val == 1)
+            return true;
+        return isonepre(root->left) || isonepre(root->right);
     }
     TreeNode* pruneTree(TreeNode* root) {
-        if(root==nullptr)return nullptr;
-        if(!isonepre(root->left))root->left = nullptr;
-        if(!isonepre(root->right))root->right = nullptr;
-
-        pruneTree(root->left);
-        pruneTree(root->right);
-
-        if(root->left==nullptr and root->right==nullptr and root->val==0){
+        if (root == nullptr)
             return nullptr;
-        }
+        // if(!isonepre(root->left))root->left = nullptr;
+        // if(!isonepre(root->right))root->right = nullptr;
+
+        // pruneTree(root->left);
+        // pruneTree(root->right);
+
+        // if(root->left==nullptr and root->right==nullptr and root->val==0){
+        //     return nullptr;
+        // }
+        // return root;
+
+        // alternative
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        if(root->left==nullptr and root->right==nullptr and root->val==0)return nullptr;
         return root;
+
     }
 };
